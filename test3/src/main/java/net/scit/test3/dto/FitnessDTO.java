3544c2337dfab6f63b4119919e139ce9c96e57b5
YoonhyeSuh <suh2185@gmail.com>
2024-12-05 15:46:24 +0900

    Update FitnessDTO.java

--- test3/src/main/java/net/scit/test3/dto/FitnessDTO.java
+++ test3/src/main/java/net/scit/test3/dto/FitnessDTO.java
@@ -23,4 +23,43 @@ public class FitnessDTO {
 	private double stdWeight;
 	private double bmi;
 	private String bmiResult;
+	
+	public void setHeight(double height) {//키를 입력받고 나서 계산
+		this.height = height;
+		calcStdWeight();
+		calcBmi();
+	}
+	
+	public void setWeight(double weight) {//몸무게를 입력받고 나서 계산
+		this.weight = weight;
+		calcStdWeight();
+		calcBmi();
+	}
+	
+	//표준체중, bmi, 결과
+	//사용자 정의 메소드
+	private void calcStdWeight() {
+		double temp = this.height / 100;	//cm -> m로 환산
+		
+		if(this.gender) {//gender가 남자
+			this.stdWeight = temp * temp * 22;	//남성의 표준체중
+		} else {//gender가 여자
+			this.stdWeight = temp * temp * 21;	//여성의 표준체중
+		}
+	}
+	
+	private void calcBmi() {
+		double temp = this.height / 100;
+		this.bmi = this.weight / (temp * temp);
+		calcBmiResult();
+	}
+	
+	private void calcBmiResult() {
+		if(this.bmi >= 35) this.bmiResult = "고도 비만";
+		else if(this.bmi >= 30) this.bmiResult = "중도 비만";
+		else if(this.bmi >= 25) this.bmiResult = "경도 비만";
+		else if(this.bmi >= 23) this.bmiResult = "과체중";
+		else if(this.bmi >= 18.5) this.bmiResult = "정상";
+		else this.bmiResult = "저체중";
+	}
 }
