6f063f7935c16b3b45d3120c47ff51bdd4ea6a0d
YoonhyeSuh <suh2185@gmail.com>
2024-12-05 17:52:05 +0900

    Update FitnessController.java

--- test3/src/main/java/net/scit/test3/controller/FitnessController.java
+++ test3/src/main/java/net/scit/test3/controller/FitnessController.java
@@ -1,17 +1,22 @@
 package net.scit.test3.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.scit.test3.dto.FitnessDTO;
 import net.scit.test3.service.FitnessService;
 
 @Controller
 @RequestMapping("/fitness")
+@Slf4j
 @RequiredArgsConstructor	//생성자 자동 생성
 public class FitnessController {
 	
@@ -25,8 +30,16 @@ public class FitnessController {
 	@PostMapping("/regist")
 	public String regist(@ModelAttribute FitnessDTO fitnessDTO) {
 		
+		log.info("================= {}", fitnessDTO.toString());
 		fitnessService.insert(fitnessDTO);
 		
 		return "redirect:/";
+	}
+	
+	@GetMapping("/selectAll")
+	public String selectAll(Model model) {
+		List<FitnessDTO> list = fitnessService.selectAll();
+		model.addAttribute("list", list);
+		return "fitness/selectAll";
 	}
 }
