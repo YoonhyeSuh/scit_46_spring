25ca41132823c6f76d4e814d45794241c09eec59
YoonhyeSuh <suh2185@gmail.com>
2024-12-05 14:46:50 +0900

    Update FitnessController.java

--- test3/src/main/java/net/scit/test3/controller/FitnessController.java
+++ test3/src/main/java/net/scit/test3/controller/FitnessController.java
@@ -1,5 +1,25 @@
 package net.scit.test3.controller;
 
-public class FitnessController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.scit.test3.dto.FitnessDTO;
+
+@Controller
+@RequestMapping("/fitness")
+public class FitnessController {
+	
+	@GetMapping("/regist")
+	public String regist() {
+		return "fitness/regist";
+	}
+	
+	@PostMapping("/regist")
+	public String regist(@ModelAttribute FitnessDTO fitnessDTO) {
+		
+		return "redirect:/";
+	}
 }
