8f93ca704e4d2050c6c0818be01f3bf48d70c989
YoonhyeSuh <suh2185@gmail.com>
2024-12-06 16:30:38 +0900

    create hw_cashbook project

---
+++ hw_cashbook/src/main/java/net/scit/hw_cashbook/HwCashbookApplication.java
@@ -0,0 +1,13 @@
+package net.scit.hw_cashbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCashbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwCashbookApplication.class, args);
+	}
+
+}
---
+++ hw_cashbook/src/test/java/net/scit/hw_cashbook/HwCashbookApplicationTests.java
@@ -0,0 +1,13 @@
+package net.scit.hw_cashbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCashbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
