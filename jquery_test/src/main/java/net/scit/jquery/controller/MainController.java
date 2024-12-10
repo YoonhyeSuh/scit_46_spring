8973cff0d68dc5b8d626a0f5cfe28b994bb0e767
YoonhyeSuh <suh2185@gmail.com>
2024-12-10 17:41:04 +0900

    Update MainController.java

--- jquery_test/src/main/java/net/scit/jquery/controller/MainController.java
+++ jquery_test/src/main/java/net/scit/jquery/controller/MainController.java
@@ -1,7 +1,12 @@
 package net.scit.jquery.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import net.scit.jquery.dto.MemberDTO;
 
 @Controller
 public class MainController {
@@ -9,6 +14,14 @@ public class MainController {
 	public String index() {
 		return "index";
 	}
+	
+	@PostMapping("/join")
+	public String join(@ModelAttribute MemberDTO member, Model model) {
+		System.out.println(member);
+		
+		model.addAttribute("member", member);
+		
+		return "index";
+	}
 }
 
-//아이디(String), 이메일(String), 사는동네(String), 나이(int): 20~50
