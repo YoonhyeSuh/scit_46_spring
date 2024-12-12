efe2883b87372f16089968f241dcc0f883de8c03
YoonhyeSuh <suh2185@gmail.com>
2024-12-12 19:31:32 +0900

    Update ListEntity.java

--- todolist/src/main/java/net/scit/todolist/entity/ListEntity.java
+++ todolist/src/main/java/net/scit/todolist/entity/ListEntity.java
@@ -1,5 +1,62 @@
 package net.scit.todolist.entity;
 
-public class ListEntity {
+import java.time.LocalDate;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import net.scit.todolist.dto.ListDTO;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+@Entity
+@Table(name="todolist")
+public class ListEntity {
+	@Id
+	@Column(name="seqno")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int seqno;
+	
+	@Column(name="regdate")
+	@CreationTimestamp
+	private LocalDate regdate;
+	
+	@Column(name="status")
+	private String status;
+	
+	@Column(name="importance")
+	private String importance;
+	
+	@Column(name="categories")
+	private String categories;
+	
+	@Column(name="todo")
+	private String todo;
+	
+	public static ListEntity toEntity(ListDTO listDTO) {
+		return ListEntity.builder()
+				.seqno(listDTO.getSeqno())
+				.regdate(listDTO.getRegdate())
+				.status(listDTO.getStatus())
+				.importance(listDTO.getImportance())
+				.categories(listDTO.getCategories())
+				.todo(listDTO.getTodo())
+				.build();
+	}
 }
