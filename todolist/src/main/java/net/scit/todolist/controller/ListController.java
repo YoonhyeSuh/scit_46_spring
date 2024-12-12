dfca1fc32bf188ffa6ef7cfb9de5fb61d2b64af4
YoonhyeSuh <suh2185@gmail.com>
2024-12-12 20:48:07 +0900

    Update ListController.java

--- todolist/src/main/java/net/scit/todolist/controller/ListController.java
+++ todolist/src/main/java/net/scit/todolist/controller/ListController.java
@@ -1,10 +1,14 @@
 package net.scit.todolist.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,13 +25,24 @@ public class ListController {
 	public final ListService listService;
 	@GetMapping("/list")
 	public String list() {
-		
 		return "todo/list";
 	}
 	
 	@PostMapping("/list")
-	public String list(@ModelAttribute ListDTO listDTO) {
-		listService.insert(listDTO);
-		return "redirect:/";
+	public String list(@ModelAttribute ListDTO listDTO, Model model) {
+		if() {
+			System.out.println("오류");
+		} else {
+			listService.insert(listDTO);
+			List<ListDTO> list = listService.selectAll();
+			model.addAttribute("list", list);
+		}
+		return "redirect:/todo/list";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam(name="id")int id, Model model) {
+		listService.delete(id);
+		return "redirect:/todo/list";
 	}
 }
