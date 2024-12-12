80899e38ed48e39733dbb3a82247a8e8cd02fd3e
YoonhyeSuh <suh2185@gmail.com>
2024-12-12 19:31:29 +0900

    Update ListController.java

--- todolist/src/main/java/net/scit/todolist/controller/ListController.java
+++ todolist/src/main/java/net/scit/todolist/controller/ListController.java
@@ -1,18 +1,24 @@
 package net.scit.todolist.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.scit.todolist.dto.ListDTO;
+import net.scit.todolist.serivce.ListService;
 
 
 @Controller
 @RequestMapping("/todo")
+@Slf4j
+@RequiredArgsConstructor
 public class ListController {
+	
+	public final ListService listService;
 	@GetMapping("/list")
 	public String list() {
 		
@@ -20,8 +26,8 @@ public class ListController {
 	}
 	
 	@PostMapping("/list")
-	public String list(@ModelAttribute ListDTO listDTO, Model model) {
-		model.addAttribute("list", listDTO);
-		return "todo/list";
+	public String list(@ModelAttribute ListDTO listDTO) {
+		listService.insert(listDTO);
+		return "redirect:/";
 	}
 }
