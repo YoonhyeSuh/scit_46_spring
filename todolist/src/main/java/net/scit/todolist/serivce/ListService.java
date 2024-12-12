c2fde08b2441a30f7f65d8378316c400098acfaf
YoonhyeSuh <suh2185@gmail.com>
2024-12-12 19:31:36 +0900

    Update ListService.java

--- todolist/src/main/java/net/scit/todolist/serivce/ListService.java
+++ todolist/src/main/java/net/scit/todolist/serivce/ListService.java
@@ -1,5 +1,21 @@
 package net.scit.todolist.serivce;
 
-public class ListService {
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.scit.todolist.dto.ListDTO;
+import net.scit.todolist.entity.ListEntity;
+import net.scit.todolist.repository.ListRepository;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ListService {
+	private final ListRepository listRepository;
+	public void insert(ListDTO listDTO) {
+		ListEntity listEntity = ListEntity.toEntity(listDTO);
+		listRepository.save(listEntity);
+	}
+	
 }
