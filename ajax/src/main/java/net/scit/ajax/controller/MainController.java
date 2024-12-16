54fdc78d8c7b74ad4a32f4212d9e3ecf5ac1bcae
YoonhyeSuh <suh2185@gmail.com>
2024-12-16 14:18:32 +0900

    Update MainController.java

--- ajax/src/main/java/net/scit/ajax/controller/MainController.java
+++ ajax/src/main/java/net/scit/ajax/controller/MainController.java
@@ -11,24 +11,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController
+//@RestController
 public class MainController {
 	@GetMapping({"/",""})
-	@ResponseBody
-	public Map<String, String> index() {
-		//return "index";	//forwarding index.html
-		
-		Map<String, String> map = new HashMap<>();
-		map.put("a01", "홍길동");
-		map.put("a02", "임꺽정");
-		map.put("a03", "전우치");
-		return map;
-		//return "index";	//forwarding index.html
+	public String index() {
+		return "index";
 	}
-	
-	@GetMapping({"/data"})
-	public List<String> test() {
-		List<String> list = Arrays.asList("사과", "딸기", "포도", "바나나");
-		return list;
-	}
+//	@ResponseBody
+//	public Map<String, String> index() {
+//		//return "index";	//forwarding index.html
+//		
+//		Map<String, String> map = new HashMap<>();
+//		map.put("a01", "홍길동");
+//		map.put("a02", "임꺽정");
+//		map.put("a03", "전우치");
+//		return map;
+//		//return "index";	//forwarding index.html
+//	}
+//	
+//	@GetMapping({"/data"})
+//	public List<String> test() {
+//		List<String> list = Arrays.asList("사과", "딸기", "포도", "바나나");
+//		return list;
+//	}
 }
