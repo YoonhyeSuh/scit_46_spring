60ccfa165f3e77b92fd490fdd6fb7cc987ae20ac
YoonhyeSuh <suh2185@gmail.com>
2024-12-18 16:24:30 +0900

    Update ListRepository.java

--- todolist/src/main/java/net/scit/todolist/repository/ListRepository.java
+++ todolist/src/main/java/net/scit/todolist/repository/ListRepository.java
@@ -1,9 +1,11 @@
 package net.scit.todolist.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.scit.todolist.entity.ListEntity;
 
 public interface ListRepository extends JpaRepository<ListEntity, Integer>{
-
+	
 }
