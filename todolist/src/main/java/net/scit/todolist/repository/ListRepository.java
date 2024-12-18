9738984ed2aa299ee1ca1d5fdc31e94e0a855c2c
YoonhyeSuh <suh2185@gmail.com>
2024-12-18 16:54:09 +0900

    Update ListRepository.java

--- todolist/src/main/java/net/scit/todolist/repository/ListRepository.java
+++ todolist/src/main/java/net/scit/todolist/repository/ListRepository.java
@@ -2,10 +2,13 @@ package net.scit.todolist.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.scit.todolist.entity.ListEntity;
 
 public interface ListRepository extends JpaRepository<ListEntity, Integer>{
+
+	List<ListEntity> findByImportanceAndCategories(String importance, String categories, Sort by);
 	
 }
