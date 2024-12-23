d8ff342cc4e55514f6940254568bd5b97a763973
YoonhyeSuh <suh2185@gmail.com>
2024-12-23 17:13:11 +0900

    Create UserRepository.java

---
+++ sec/src/main/java/net/scit/sec/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package net.scit.sec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.scit.sec.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+	
+}
