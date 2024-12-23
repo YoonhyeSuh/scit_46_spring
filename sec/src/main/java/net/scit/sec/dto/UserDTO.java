8e0e08038333dce17c91f39bfc5634439f6e6e3f
YoonhyeSuh <suh2185@gmail.com>
2024-12-23 17:13:07 +0900

    Create UserDTO.java

---
+++ sec/src/main/java/net/scit/sec/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package net.scit.sec.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class UserDTO {
+	private Integer seq;
+	private String userId;
+	private String userPwd;
+	private String userName;
+}
