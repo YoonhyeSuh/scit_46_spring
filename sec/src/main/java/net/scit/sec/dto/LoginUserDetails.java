621ac061a14df958dd9bfe9ebdc1ebac25bdd127
YoonhyeSuh <suh2185@gmail.com>
2024-12-24 17:31:55 +0900

    Create LoginUserDetails.java

---
+++ sec/src/main/java/net/scit/sec/dto/LoginUserDetails.java
@@ -0,0 +1,51 @@
+package net.scit.sec.dto;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class LoginUserDetails implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	private Integer seq;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role));	//collection > list > arraylist
+	}
+
+	@Override
+	public String getPassword() {
+		return this.userPwd;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.userId;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+
+}
