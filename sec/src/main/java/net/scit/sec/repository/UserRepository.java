0f6ad38ba69a9c3a1f44abddb8cd0f8d6e545a9d
YoonhyeSuh <suh2185@gmail.com>
2024-12-24 14:58:38 +0900

    Update UserRepository.java

--- sec/src/main/java/net/scit/sec/repository/UserRepository.java
+++ sec/src/main/java/net/scit/sec/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import net.scit.sec.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+	boolean existsByUserId(String userId);
 	
 }
