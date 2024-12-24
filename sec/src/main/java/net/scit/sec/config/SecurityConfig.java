d473752bb73742069fc59bf3afcb7ee13af045fb
YoonhyeSuh <suh2185@gmail.com>
2024-12-24 17:31:50 +0900

    Update SecurityConfig.java

--- sec/src/main/java/net/scit/sec/config/SecurityConfig.java
+++ sec/src/main/java/net/scit/sec/config/SecurityConfig.java
@@ -18,12 +18,21 @@ public class SecurityConfig {
 								"/join",
 								"/joinProc",
 								"/idCheck",
+								"/favicon.ico",
 								"/images/**",
 								"/js/**",
 								"/css/**").permitAll()					//인증절차없이 접근
 				.requestMatchers("/admin").hasRole("ADMIN")				//admin으로 인증될때만 접근가능
 				.requestMatchers("/my/**").hasAnyRole("ADMIN","USER")	//ROLE_ADMIN /my로 시작하는 요청은 ADMIN과 USER 접속할 수 있음
 				.anyRequest().authenticated());							//기타 다른 경로는 인증된 사용자만 접근 가능
+		
+		//Custom 로그인
+		http.formLogin((auth) -> auth
+				.loginPage("/login")					//로그인 화면 요청
+				.loginProcessingUrl("/loginProc")	//로그인 처리 요청
+				.usernameParameter("userId")		//사용자가 재정의한 아이디 파라미터
+				.passwordParameter("userPwd")		//사용자가 재정의한 비번 파라미터
+				.permitAll());
 		
 		//POST 요청 시 CSRF(Cross-Site request Forgery) 토근을 요청함
 		http.csrf((auth) -> auth.disable());
