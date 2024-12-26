fadfb27596639e914826fc188ab500521ffc560e
YoonhyeSuh <suh2185@gmail.com>
2024-12-26 14:50:54 +0900

    Update UserRepository.java

--- sec/src/main/java/net/scit/sec/repository/UserRepository.java
+++ sec/src/main/java/net/scit/sec/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.scit.sec.entity.UserEntity;
 
+//jparepository를 상속받기 때문에 @Repository를 안써도 됨
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
 	boolean existsByUserId(String userId);
