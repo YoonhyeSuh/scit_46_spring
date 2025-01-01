0d27f2cb6954689efc1bba684ebc5810fe83d2fb
YoonhyeSuh <suh2185@gmail.com>
2025-01-01 20:59:32 +0900

    feat.게시글 목록 수정 업데이트

--- spring7/src/main/java/net/scit/spring7/controller/BoardController.java
+++ spring7/src/main/java/net/scit/spring7/controller/BoardController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +22,12 @@ import net.scit.spring7.service.BoardService;
 public class BoardController {
 	
 	private final BoardService boardService;
+	
+	/**
+	 * 게시글 목록 요청
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("/boardList")
 	public String boardList(Model model) {
 		
@@ -28,8 +37,79 @@ public class BoardController {
 		return "board/boardList";
 	}
 	
+	/**
+	 * 게시글 화면 요청
+	 * @return
+	 */
 	@GetMapping("/boardWrite")
 	public String boardWrite() {
 		return "board/boardWrite";
+	}
+	
+	/**
+	 * 게시글쓰기 처리 요청
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/boardWrite")
+	public String boardWrite(@ModelAttribute BoardDTO boardDTO) {
+		//DB 등록
+		log.info("================ {}", boardDTO.toString());
+		boardService.insertBoard(boardDTO);
+		
+		
+		return "redirect:/board/boardList";
+	}
+	
+	/**
+	 * 조회수 증가
+	 * @param boardSeq
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/boardDetail")
+	public String boardDetail(@RequestParam(name="boardSeq")Long boardSeq, Model model) {//Model model은 데이터를 가져온다
+		//DB에서 boardSeq에 해당하는 하나의 게시글을 조회
+		
+		BoardDTO boardDTO = boardService.selectOne(boardSeq);
+		boardService.incrementHitcount(boardSeq);
+		model.addAttribute("board", boardDTO);
+		return "board/boardDetail";
+	}
+	
+	/**
+	 * 삭제를 위한 요청
+	 * @param boardSeq
+	 * @return
+	 */
+	@GetMapping("/boardDelete")
+	public String boardDelete(@RequestParam(name="boardSeq")Long boardSeq) {
+		boardService.deleteOne(boardSeq);
+		return "redirect:/board/boardList";
+	}
+	
+	/**
+	 * 수정을 위한 화면 요청
+	 * @param boardSeq
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/boardUpdate")
+	public String boardUpdate(@RequestParam(name="boardSeq")Long boardSeq, Model model) {
+		BoardDTO boardDTO = boardService.selectOne(boardSeq);
+		model.addAttribute("board", boardDTO);
+		return "board/boardUpdate";
+	}
+	
+	/**
+	 * 게시글 수정 처리 요청
+	 * @param boardDTO
+	 * @return
+	 */
+	@PostMapping("/boardUpdate")
+	public String boardUpdate(@ModelAttribute BoardDTO boardDTO) {//데이터가 4개 날라와서 한꺼번에 받음
+		log.info("==== 수정데이터: {}", boardDTO.toString());	
+		boardService.updateBoard(boardDTO);
+		return "redirect:/board/boardList";
 	}
 }
--- spring7/src/main/java/net/scit/spring7/service/BoardService.java
+++ spring7/src/main/java/net/scit/spring7/service/BoardService.java
@@ -2,9 +2,12 @@ package net.scit.spring7.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.scit.spring7.dto.BoardDTO;
@@ -18,7 +21,7 @@ public class BoardService {
 	private final BoardRepository boardRepository;
 	
 	public List<BoardDTO> selectAll(){
-		List<BoardEntity> temp = boardRepository.findAll();
+		List<BoardEntity> temp = boardRepository.findAll(Sort.by(Sort.Direction.DESC, "createDate"));
 		List<BoardDTO> list = new ArrayList<>();
 		
 		log.info("============= 총 글개수: {}", temp.size());
@@ -26,4 +29,121 @@ public class BoardService {
 		temp.forEach((entity) -> list.add(BoardDTO.toDTO(entity)));
 		return list;
 	}
+	
+	/**
+	 * 전달받은 BoardDTO를 DB에 저장
+	 * @param boardDTO
+	 */
+	public void insertBoard(BoardDTO boardDTO) {
+		BoardEntity entity = BoardEntity.toEntity(boardDTO);
+		
+		boardRepository.save(entity);
+	}
+
+	/**
+	 * boardSeq에 해당하는 글을 조회
+	 * @param boardSeq
+	 * @return
+	 */
+	public BoardDTO selectOne(Long boardSeq) {
+		Optional<BoardEntity> temp = boardRepository.findById(boardSeq); //findById는 Optional로 반환
+		
+		//조회된 결과를 꺼내서 dto로 반환
+		/*if(temp.isPresent()) return null;	//데이터가 없으면 null 반환
+		BoardEntity entity = temp.get();
+		return BoardDTO.toDTO(entity);*/
+		
+		return BoardDTO.toDTO(temp.get());
+	}
+
+	/*
+		boardSeq레코드 조회수 증가
+	 	jpa는 update() 메소드가 따로 없음
+		save() -> 기존에 있던 값을 꺼내서 1을 증가하고 다시 집어 넣는다
+	 	update from board set hitcount = hitcount+1 where board_seq = ?;
+	  
+	  	save() : 새롭게 데이터를 imsert
+	  		  : 기존 데이터일 경우 update
+	  	그 기준이 pk(pk가 중요)
+	  
+	  	하나의 트랜잭션내에서 pk로 조회한 후 set을 하면서 값을 바꾸면 update
+	  
+	  	delete from board
+	  	where board_seq = ?;
+	 */
+	
+	/**
+	 * 1) 조회 : findById(boardSeq)
+	 * 2) hitCount를 get한후 +1 ==> set
+	 * @param boardSeq
+	 */
+	
+	@Transactional
+	public void incrementHitcount(Long boardSeq) {
+		Optional<BoardEntity> temp = boardRepository.findById(boardSeq);
+		
+		if(!temp.isPresent()) return;
+		BoardEntity boardEntity = temp.get();
+		
+		int hitcount = boardEntity.getHitCount() + 1; //get 꺼내진 데이터 타입은 entity
+		boardEntity.setHitCount(hitcount);
+	}
+
+	/**
+	 * DB에 삭제 요청
+	 * @param boardSeq
+	 */
+	@Transactional
+	public void deleteOne(Long boardSeq) {
+		Optional<BoardEntity> temp = boardRepository.findById(boardSeq);
+		
+		if(!temp.isPresent()) return;
+		boardRepository.deleteById(boardSeq);
+	}
+
+	/*
+		수정
+		1) 목록에서 수정요청시 --> boardSeq를 request param으로 보내서 조회
+		2) 조회된 Board 결과값을 model에 저장하여 controller에 넣는다
+		3) 수정화면에서 2)의 데이터를 출력 input 내에서 출력하면 된다
+		4) 사용자가 값을 수정하고, 그 갑으로 수정 요청을 보냄
+		
+		update
+	        board 
+	    set
+	        board_content=????,
+	        board_title=????,
+	        board_writer=?,	//콘트롤러에서 시큐리티 세션에 접근한 후에 그 값으로 세팅
+	        create_date=?,
+	        hit_count=?,
+	        update_date=? 
+	    where
+	        board_seq=????
+	 */
+	
+	/**
+	 * DB에 수정처리
+	 * @param boardDTO
+	 */
+	@Transactional
+	public void updateBoard(BoardDTO boardDTO) {
+		//1) 수정하려는 데이터가 있는지 확인
+		Optional<BoardEntity> temp = boardRepository.findById(boardDTO.getBoardSeq());
+		
+		if(!temp.isPresent()) return;	//없으면 return
+		//2) 있으면 dto->entity로 변환
+//		BoardEntity entity = BoardEntity.toEntity(boardDTO);
+//		boardRepository.save(entity); 	//모든 값이 set됨
+		
+		BoardEntity boardEntity = temp.get();
+		boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+		boardEntity.setBoardContent(boardDTO.getBoardContent());
+		
+		boardRepository.save(boardEntity);
+	
+	}
+	
+	
 }
+
+
