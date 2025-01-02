d1102ad567bed6e0d18898605cd2e586d184c494
YoonhyeSuh <suh2185@gmail.com>
2025-01-02 17:27:22 +0900

    Update BoardRepository.java

--- spring7/src/main/java/net/scit/spring7/repository/BoardRepository.java
+++ spring7/src/main/java/net/scit/spring7/repository/BoardRepository.java
@@ -1,9 +1,19 @@
 package net.scit.spring7.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.scit.spring7.entity.BoardEntity;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+
+	//검색기능을 위한 쿼리 메소드 선
+	List<BoardEntity> findByBoardTitleContains(String searchWord, Sort sort);
+
+	List<BoardEntity> findByBoardWriterContains(String searchWord, Sort sort);
+
+	List<BoardEntity> findByBoardContentContains(String searchWord, Sort sort);
 
 }
