8fb90ac8c3ef8b97cfdc6aed4f2adc4fa4712674
YoonhyeSuh <suh2185@gmail.com>
2025-01-06 15:30:56 +0900

    Update UserController.java

--- spring7/src/main/java/net/scit/spring7/controller/UserController.java
+++ spring7/src/main/java/net/scit/spring7/controller/UserController.java
@@ -2,16 +2,23 @@ package net.scit.spring7.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import net.scit.spring7.dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.scit.spring7.service.UserService;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j	//log찍어서 데이터 넘어오는지 확인
+@RequiredArgsConstructor
 public class UserController {
 
+	private final UserService userService;
+	
 	@GetMapping("/join")
 	public String join() {
 		return "user/join";
@@ -25,5 +32,12 @@ public class UserController {
 	@GetMapping("/login")
 	public String login() {
 		return "user/login";
+	}
+	
+	@PostMapping("/idCheck")
+	@ResponseBody
+	public boolean idCheck(@RequestParam(name="userId")String userId) {
+		boolean result = userService.existId(userId);
+		return true;
 	}
 }
