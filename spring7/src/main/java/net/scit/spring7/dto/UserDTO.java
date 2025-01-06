af854044bcde012ef1abf041352741f8a35425a3
YoonhyeSuh <suh2185@gmail.com>
2025-01-06 15:30:58 +0900

    Update UserDTO.java

--- spring7/src/main/java/net/scit/spring7/dto/UserDTO.java
+++ spring7/src/main/java/net/scit/spring7/dto/UserDTO.java
@@ -1,5 +1,12 @@
 package net.scit.spring7.dto;
 
+
 public class UserDTO {
 
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String email;
+	private String roles;
+	private Boolean enabled;
 }
