20cad1e64f9eaa17c024e04a04dc16a9bd267dcf
YoonhyeSuh <suh2185@gmail.com>
2025-01-08 17:21:48 +0900

    Update BoardController.java

--- spring7/src/main/java/net/scit/spring7/controller/BoardController.java
+++ spring7/src/main/java/net/scit/spring7/controller/BoardController.java
@@ -1,21 +1,30 @@
 package net.scit.spring7.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.scit.spring7.dto.BoardDTO;
 import net.scit.spring7.service.BoardService;
+import net.scit.spring7.util.FileService;
 
 @Controller
 @RequestMapping("/board")
@@ -24,6 +33,8 @@ import net.scit.spring7.service.BoardService;
 public class BoardController {
 	
 	private final BoardService boardService;
+	@Value("${spring.servlet.multipart.location}")
+	private String uploadPath;
 	
 	/**
 	 * 게시글 목록 요청
@@ -115,7 +126,7 @@ public class BoardController {
 	 * @return
 	 */
 	@GetMapping("/boardUpdate")
-	public String boardUpdate(@RequestParam(name="boardSeq")Long boardSeq, @RequestParam(name="searchItem", defaultValue = "boardTitle")String searchItem, @RequestParam(name="searchWord", defaultValue = "")String searchWord, Model model) {
+	public String boardUpdate(@ModelAttribute BoardDTO boardDTO, @RequestParam(name="boardSeq")Long boardSeq, @RequestParam(name="searchItem", defaultValue = "boardTitle")String searchItem, @RequestParam(name="searchWord", defaultValue = "")String searchWord, Model model) {
 		BoardDTO boardDTO = boardService.selectOne(boardSeq);
 		model.addAttribute("board", boardDTO);
 		model.addAttribute("searchItem", searchItem);
@@ -135,5 +146,70 @@ public class BoardController {
 		redirectAttributes.addAttribute("searchItem", searchItem);
 		redirectAttributes.addAttribute("searchWord", searchWord);
 		return "redirect:/board/boardList";
+	}
+	
+	/**
+	 * 쓰레기통 아이콘을 클릭하여 파일만 삭제하는 작업
+	 * @param boardSeq
+	 * @return
+	 */
+	
+	@GetMapping("/deleteFile")
+	public String deleteFile(@RequestParam(name="boardSeq")Long boardSeq, @RequestParam(name="searchItem", defaultValue = "boardTitle")String searchItem, @RequestParam(name="searchWord", defaultValue = "")String searchWord, RedirectAttributes rttr) {
+		
+		// 2) DB도 수정 -> file 컬럼 두개의 값을 null로
+		BoardDTO boardDTO = boardService.selectOne(boardSeq);
+		
+		String savedFileName = boardDTO.getSavedFileName();
+		String fullPath = uploadPath + "/" + savedFileName;
+		
+		// 1) 물리적으로 존재하는 파일을 삭제
+		boolean result = FileService.deleteFile(fullPath);
+		log.info("삭제결과: {}", result);
+		
+		// 2) DB도 수정 -> file 컬럼 두개의 값을 null로
+		boardService.deleteFile(boardSeq);
+		
+		//메소드 호출시 필요한 리퀘스트 파라미트를 rttr에 넣어서 가져온다
+		rttr.addAttribute("boardSeq", boardSeq);
+		rttr.addAttribute("searchItem", searchItem);
+		rttr.addAttribute("searchWord", searchWord);
+		//서치아이템, 서치워드도 보내야함
+		//클라이언트에서 보내야함
+		return "redirect:/board/boardDetail";
+	}
+	
+	@GetMapping("/download")
+	public String download(@RequestParam(name="boardSeq")Long boardSeq, HttpServletResponse response) {
+		BoardDTO boardDTO = boardService.selectOne(boardSeq);
+		String savedFileName = boardDTO.getSavedFileName();
+		String originalFileName = boardDTO.getOriginalFileName();
+				
+		try {
+			String tempName = URLEncoder.encode(originalFileName, StandardCharsets.UTF_8.toString());
+			response.setHeader("Content-Disposition", "attachment;filename=" + tempName);
+		} catch (UnsupportedEncodingException e) {
+			
+			e.printStackTrace();
+		}
+		
+		String fullPath = uploadPath + "/" + savedFileName;
+
+		FileInputStream fin = null;				//로컬에서 업로드
+		ServletOutputStream fout = null;		//네트워크로 output
+		
+		try {
+			fin = new FileInputStream(fullPath);
+			fout = response.getOutputStream();
+			
+			FileCopyUtils.copy(fin, fout);	//스트림?(링)으로 데이터를 내려보낸다
+			
+			fout.close();	//리소스 정리
+			fin.close();	
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		return null;	//다운로드 기능만 수행하면 되서 null로 리턴해도 상관없음
 	}
 }
